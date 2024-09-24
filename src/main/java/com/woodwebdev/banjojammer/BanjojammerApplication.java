02fda11084a0a42b39e97e028fdf11507cdc978a
John Wood <johnwood@Johns-Air.attlocal.net>
2024-09-24 17:11:30 -0500

    First commit

---
+++ src/main/java/com/woodwebdev/banjojammer/BanjojammerApplication.java
@@ -0,0 +1,13 @@
+package com.woodwebdev.banjojammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanjojammerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanjojammerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woodwebdev/banjojammer/BanjojammerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woodwebdev.banjojammer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanjojammerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
